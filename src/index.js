6fadd6fe0e423f06e9f8675c6386bb6dbcfb3666
Muhmmd-Usama <mummdusama@gmail.com>
2022-10-11 21:13:06 +0200

    Fix data response

--- src/index.js
+++ src/index.js
@@ -1,32 +1,46 @@
 import './index.css';
 
-const form = document.getElementById('form-score'),
+const leaderForm = document.getElementById('form-score'),
   leaderName = document.getElementById('name'),
-  leaderScore = document.getElementById('form-score');
+  leaderScore = document.getElementById('score');
 
-const sendingData = (e) => {
-  e.preventDefault();
-  const leaderData = {
-    user: leaderName.value,
-    score: leaderScore.value,
-  };
-  sendLeaderScore(leaderData)
+const sendingData = () => {
+    const leaderData = {
+        user: leaderName.value,
+        score: leaderScore.value,
+    };
+    sendLeaderScore(leaderData);
+    console.log(leaderData);
+      leaderName.value = '';
+      leaderScore.value = '';
 };
 
-form.addEventListener('submit', sendingData);
+leaderForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    sendingData()
+});
 
 // https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/leaderProj08ScorE00Mit/scores/
+const apiURL =
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/leaderProj08ScorE00Mit/scores/';
 
-const sendLeaderScore = (leaderData) => {
-  fetch(
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/leaderProj08ScorE00Mit/scores/',
-    {
-      method: 'POST',
-      body: JSON.stringify(leaderData),
-      headers: { 'Content-type': 'application/json; charset=UTF-8' },
-    }
-  );
+const sendLeaderScore = async (leaderData) => {
+  await fetch(apiURL, {
+    method: 'POST',
+    body: JSON.stringify(leaderData),
+    headers: { 'Content-type': 'application/json; charset=UTF-8' },
+  });
 };
 
-
-
+const receivedData = async () => {
+  try {
+    const response = await fetch(apiURL);
+    if (response.status === 200) {
+      const data = await response.json();
+      updateUi(data);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+receivedData();
